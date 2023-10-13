1176555296aac3c6358b80c675cddf68b19120c8
kimwoojin <kimwoojin1903@gmail.com>
2023-10-13 10:51:30 +0900

    응답페이지의 컨텐츠타입 설정

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 
+const contentType = {
+    'Content-Type': 'text/html',
+};
+
 http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, contentType);
