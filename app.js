01c09d10d51bade08888104aa0f16ce063abdb70
kimwoojin <kimwoojin1903@gmail.com>
2023-10-13 10:56:40 +0900

    응답 페이지 호출 함수 생성

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const contentType = {
 http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, contentType);
+        fs.readFile('index.html', (err, data) => {});
     } else {
         res.writeHead(404, contentType);
     }
