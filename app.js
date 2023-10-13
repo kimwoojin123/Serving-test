1f32e2c05ae833b38c5f5a4ee289d2f6de72aa02
kimwoojin <kimwoojin1903@gmail.com>
2023-10-13 10:47:55 +0900

    url 주소에 따른 응답기준 설정

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) => {});
+http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+    } else {
+    }
+});
