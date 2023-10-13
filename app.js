b52c3d42942965f0e959fbb6d7f637722b75cbe1
kimwoojin <kimwoojin1903@gmail.com>
2023-10-13 11:06:07 +0900

    컨텐츠타입의 유니코드설정 수정

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const contentType = {
-    'Content-Type': 'text/html',
-    charset: 'utf-8',
+    'Content-Type': 'text/html;charset=utf-8',
 };
 
 http.createServer((req, res) => {
